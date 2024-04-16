0f63b3ee6288c593e69b89ae58a132326b59be64
vact19 <chj6703@gmail.com>
2024-04-16 18:17:45 +0900

    !Update: ApiResponseDto를 제거하여 응답 템플릿 통일. 응답값 Enum의 위치를 global -> api로 이동
    EmailController의 응답 json field 중 'code' -> 'statusCode' 이름 변경됨.

--- src/main/java/com/dominest/dominestbackend/api/user/component/email/controller/EmailController.java
+++ src/main/java/com/dominest/dominestbackend/api/user/component/email/controller/EmailController.java
@@ -1,11 +1,11 @@
 package com.dominest.dominestbackend.api.user.component.email.controller;
 
+import com.dominest.dominestbackend.api.common.ResponseTemplate;
 import com.dominest.dominestbackend.api.user.component.email.request.EmailRequest;
 import com.dominest.dominestbackend.domain.user.component.email.service.EmailService;
 import com.dominest.dominestbackend.domain.user.component.email.service.EmailVerificationService;
-import com.dominest.dominestbackend.global.apiResponse.ApiResponseDto;
-import com.dominest.dominestbackend.global.apiResponse.ErrorStatus;
-import com.dominest.dominestbackend.global.apiResponse.SuccessStatus;
+import com.dominest.dominestbackend.api.user.component.email.responsestatus.ErrorStatus;
+import com.dominest.dominestbackend.api.user.component.email.responsestatus.SuccessStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,25 +23,37 @@ public class EmailController {
     private final EmailVerificationService emailVerificationService;
 
     @PostMapping("/send") // 인증번호 발송 버튼 누르면 메일 가게
-    public ResponseEntity<ApiResponseDto<String>> sendEmail(@RequestBody EmailRequest emailRequest) {
+    public ResponseEntity<ResponseTemplate<String>> sendEmail(@RequestBody EmailRequest emailRequest) {
         emailService.sendJoinMessage(emailRequest.getEmail()); // 이메일로 인증코드를 보낸다.
-        return ResponseEntity.ok(ApiResponseDto.success(SuccessStatus.SEND_EMAIL_SUCCESS, emailRequest.getEmail() + "로 검증코드를 전송했습니다."));
+
+        SuccessStatus successStatus = SuccessStatus.SEND_EMAIL_SUCCESS;
+        ResponseTemplate<String> result = new ResponseTemplate<>(successStatus.getHttpStatus(), successStatus.getMessage()
+                , emailRequest.getEmail() + "로 검증코드를 전송했습니다.");
+        return ResponseEntity.ok(result);
     }
 
-
     @PostMapping("/change/password") // 임시 비밀번호 이메일 전송
-    public ResponseEntity<ApiResponseDto<String>> changePasswordEmail(@RequestBody EmailRequest emailRequest) {
+    public ResponseEntity<ResponseTemplate<String>> changePasswordEmail(@RequestBody EmailRequest emailRequest) {
         emailService.sendChangeMessage(emailRequest.getEmail()); // 이메일로 인증코드를 보냄
-        return ResponseEntity.ok(ApiResponseDto.success(SuccessStatus.SEND_EMAIL_SUCCESS, emailRequest.getEmail() + "로 검증코드를 전송했습니다."));
+
+        SuccessStatus successStatus = SuccessStatus.SEND_EMAIL_SUCCESS;
+        ResponseTemplate<String> result = new ResponseTemplate<>(successStatus.getHttpStatus(), successStatus.getMessage(), emailRequest.getEmail() + "로 검증코드를 전송했습니다.");
+        return ResponseEntity.ok(result);
     }
 
 
     @PostMapping("/verify/code") // 이메일 인증코드 검증
-    public ResponseEntity<ApiResponseDto<String>> verifyEmail(@RequestBody EmailRequest emailRequest) {
-        if (emailVerificationService.verifyCode(emailRequest.getEmail(), emailRequest.getCode())) { // 인증 성공
-            return ResponseEntity.ok(ApiResponseDto.success(SuccessStatus.VERIFY_EMAIL_SUCCESS)); // 이메일과 함께 성공 응답 반환
+    public ResponseEntity<ResponseTemplate<String>> verifyEmail(@RequestBody EmailRequest emailRequest) {
+        boolean success = emailVerificationService.verifyCode(emailRequest.getEmail(), emailRequest.getCode());
+        if (success) { // 인증 성공
+            SuccessStatus successStatus = SuccessStatus.VERIFY_EMAIL_SUCCESS;
+            ResponseTemplate<String> result = new ResponseTemplate<>(successStatus.getHttpStatus(), successStatus.getMessage());
+            return ResponseEntity.ok(result); // 이메일과 함께 성공 응답 반환
         } else { // 인증 실패
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponseDto.error(ErrorStatus.VERIFY_EMAIL_FAILED)); // 400 Bad Req 상태로 실패 응답 반환
+            ErrorStatus errorStatus = ErrorStatus.VERIFY_EMAIL_FAILED;
+            ResponseTemplate<String> result = new ResponseTemplate<>(errorStatus.getHttpStatus(), errorStatus.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST) // 400 Bad Req 상태로 실패 응답 반환
+                    .body(result);
         }
     }
 }
