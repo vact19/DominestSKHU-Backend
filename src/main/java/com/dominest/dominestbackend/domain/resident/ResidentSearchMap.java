56bc3b19fb515a9391d487034a7fb6e0a30480d3
vact19 <chj6703@gmail.com>
2024-04-22 19:34:52 +0900

    Add. ResidentSearchMap, RoomSearchMap. 다량의 데이터를 DB로부터 캐싱하기 위함

---
+++ src/main/java/com/dominest/dominestbackend/domain/resident/ResidentSearchMap.java
@@ -0,0 +1,47 @@
+package com.dominest.dominestbackend.domain.resident;
+
+import com.dominest.dominestbackend.domain.resident.component.ResidenceSemester;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ResidentSearchMap {
+    private final Map<String, Resident> sameNameSearchMap;
+    private final Map<String, Resident> samePersonSearchMap;
+
+    public static ResidentSearchMap from(List<Resident> residents) {
+        return new ResidentSearchMap(residents);
+    }
+
+    private ResidentSearchMap(List<Resident> residents) {
+        this.sameNameSearchMap = residents
+                .stream()
+                .collect(Collectors.toUnmodifiableMap(
+                        resident -> resident.getResidenceSemester() + resident.getPersonalInfo().getName()
+                        , resident -> resident)
+                );
+        this.samePersonSearchMap = residents.stream()
+                .collect(Collectors.toUnmodifiableMap(
+                        resident -> resident.getResidenceSemester()
+                                + resident.getStudentInfo().getStudentId()
+                                + resident.getPersonalInfo().getPhoneNumber().getValue()
+                                + resident.getPersonalInfo().getName()
+                        , resident -> resident)
+                );
+    }
+    /** 학기 내 동명이인 검사용 */
+    public boolean existsSameNameInSemester(ResidenceSemester residenceSemester, String name) {
+        return sameNameSearchMap.containsKey(residenceSemester + name);
+    }
+
+    /** 학기 내 동일인 검사용 */
+    public boolean existsSameResidentInSemester(Resident resident) {
+        return samePersonSearchMap.containsKey(
+                resident.getResidenceSemester()
+                        + resident.getStudentInfo().getStudentId()
+                        + resident.getPersonalInfo().getPhoneNumber().getValue()
+                        + resident.getPersonalInfo().getName()
+        );
+    }
+}
