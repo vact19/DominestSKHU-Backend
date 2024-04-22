757c8abc5963a4119555f424b39bea29c65c8e45
vact19 <chj6703@gmail.com>
2024-04-23 00:54:21 +0900

    Fix. form-data 요청에 맞는 요청 DTO 설정
    @NoArgs 대신 @AllArgs 설정함.

--- src/main/java/com/dominest/dominestbackend/api/resident/request/ExcelUploadRequest.java
+++ src/main/java/com/dominest/dominestbackend/api/resident/request/ExcelUploadRequest.java
@@ -1,15 +1,15 @@
 package com.dominest.dominestbackend.api.resident.request;
 
 import com.dominest.dominestbackend.domain.resident.component.ResidenceSemester;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
 
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class ExcelUploadRequest {
     @NotNull(message = "엑셀파일을 첨부해주세요.")
     MultipartFile file;
