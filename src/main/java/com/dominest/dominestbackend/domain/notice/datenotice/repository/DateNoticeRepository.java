d647cc37570a88b06b41a8cc264e502c1d0f4079
vact19 <chj6703@gmail.com>
2024-05-22 13:35:58 +0900

    Remove. 무의미한 Overriding 제거

--- src/main/java/com/dominest/dominestbackend/domain/notice/datenotice/repository/DateNoticeRepository.java
+++ src/main/java/com/dominest/dominestbackend/domain/notice/datenotice/repository/DateNoticeRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DateNoticeRepository extends JpaRepository<DateNotice, Long> {
-    DateNotice save(DateNotice dateNotice);
-
     List<DateNotice> findByCreatedBy(String createdBy);
 }
